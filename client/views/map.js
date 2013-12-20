2beda2bdc2191d9054c3f045b8009b056896396b
Pallavi Anderson <pkaushik@alum.mit.edu>
2013-12-19 21:36:10 -0600

    get ready to reintroduce d3 overlay layer

--- client/views/map.js
+++ client/views/map.js
@@ -35,71 +35,75 @@ Template.map.rendered = function () {
   }).resize();
   
   LeafletLib.initialize($("#map_canvas")[0], [ 41.8781136, -87.66677956445312 ], 13); 
+  var svg     = d3.select(LeafletLib.map.getPanes().overlayPane).append("svg");
+  var circle  = svg.append("circle").attr("class", "callout").attr("cx", "-100").attr("cy", "-100");
+  var zoom    = svg.append("g").attr("class", "leaflet-zoom-hide");
+  var circles = svg.append("g").attr("class", "circles");
+  var labels  = svg.append("g").attr("class", "labels");
   
+  var self = this;
+  self.node = self.find("svg");
   
-  // var self = this;
-  // self.node = self.find("svg");
-  // 
-  // if (! self.handle) {
-  //   self.handle = Meteor.autorun(function () {
-  //     var selected = Session.get('selected');
-  //     var selectedParty = selected && Parties.findOne(selected);
-  //     var radius = function (party) {
-  //       return 10 + Math.sqrt(attending(party)) * 10;
-  //     };
-  // 
-  //     // Draw a circle for each party
-  //     var updateCircles = function (group) {
-  //       group.attr("id", function (party) { return party._id; })
-  //       .attr("cx", function (party) { return party.x * 500; })
-  //       .attr("cy", function (party) { return party.y * 500; })
-  //       .attr("r", radius)
-  //       .attr("class", function (party) {
-  //         return party.public ? "public" : "private";
-  //       })
-  //       .style('opacity', function (party) {
-  //         return selected === party._id ? 1 : 0.6;
-  //       });
-  //     };
-  // 
-  //     var circles = d3.select(self.node).select(".circles").selectAll("circle")
-  //       .data(Parties.find().fetch(), function (party) { return party._id; });
-  // 
-  //     updateCircles(circles.enter().append("circle"));
-  //     updateCircles(circles.transition().duration(250).ease("cubic-out"));
-  //     circles.exit().transition().duration(250).attr("r", 0).remove();
-  // 
-  //     // Label each with the current attendance count
-  //     var updateLabels = function (group) {
-  //       group.attr("id", function (party) { return party._id; })
-  //       .text(function (party) {return attending(party) || '';})
-  //       .attr("x", function (party) { return party.x * 500; })
-  //       .attr("y", function (party) { return party.y * 500 + radius(party)/2 })
-  //       .style('font-size', function (party) {
-  //         return radius(party) * 1.25 + "px";
-  //       });
-  //     };
-  // 
-  //     var labels = d3.select(self.node).select(".labels").selectAll("text")
-  //       .data(Parties.find().fetch(), function (party) { return party._id; });
-  // 
-  //     updateLabels(labels.enter().append("text"));
-  //     updateLabels(labels.transition().duration(250).ease("cubic-out"));
-  //     labels.exit().remove();
-  // 
-  //     // Draw a dashed circle around the currently selected party, if any
-  //     var callout = d3.select(self.node).select("circle.callout")
-  //       .transition().duration(250).ease("cubic-out");
-  //     if (selectedParty)
-  //       callout.attr("cx", selectedParty.x * 500)
-  //       .attr("cy", selectedParty.y * 500)
-  //       .attr("r", radius(selectedParty) + 10)
-  //       .attr("class", "callout")
-  //       .attr("display", '');
-  //     else
-  //       callout.attr("display", 'none');
-  //   });
-  // }
+  if (! self.handle) {
+    self.handle = Meteor.autorun(function () {
+      var selected = Session.get('selected');
+      var selectedParty = selected && Parties.findOne(selected);
+      var radius = function (party) {
+        return 10 + Math.sqrt(attending(party)) * 10;
+      };
+  
+      // Draw a circle for each party
+      var updateCircles = function (group) {
+        group.attr("id", function (party) { return party._id; })
+        .attr("cx", function (party) { return party.x * 500; })
+        .attr("cy", function (party) { return party.y * 500; })
+        .attr("r", radius)
+        .attr("class", function (party) {
+          return party.public ? "public" : "private";
+        })
+        .style('opacity', function (party) {
+          return selected === party._id ? 1 : 0.6;
+        });
+      };
+  
+      var circles = d3.select(self.node).select(".circles").selectAll("circle")
+        .data(Parties.find().fetch(), function (party) { return party._id; });
+  
+      updateCircles(circles.enter().append("circle"));
+      updateCircles(circles.transition().duration(250).ease("cubic-out"));
+      circles.exit().transition().duration(250).attr("r", 0).remove();
+  
+      // Label each with the current attendance count
+      var updateLabels = function (group) {
+        group.attr("id", function (party) { return party._id; })
+        .text(function (party) {return attending(party) || '';})
+        .attr("x", function (party) { return party.x * 500; })
+        .attr("y", function (party) { return party.y * 500 + radius(party)/2 })
+        .style('font-size', function (party) {
+          return radius(party) * 1.25 + "px";
+        });
+      };
+  
+      var labels = d3.select(self.node).select(".labels").selectAll("text")
+        .data(Parties.find().fetch(), function (party) { return party._id; });
+  
+      updateLabels(labels.enter().append("text"));
+      updateLabels(labels.transition().duration(250).ease("cubic-out"));
+      labels.exit().remove();
+  
+      // Draw a dashed circle around the currently selected party, if any
+      var callout = d3.select(self.node).select("circle.callout")
+        .transition().duration(250).ease("cubic-out");
+      if (selectedParty)
+        callout.attr("cx", selectedParty.x * 500)
+        .attr("cy", selectedParty.y * 500)
+        .attr("r", radius(selectedParty) + 10)
+        .attr("class", "callout")
+        .attr("display", '');
+      else
+        callout.attr("display", 'none');
+    });
+  }
 };
 
 Template.map.destroyed = function () {
