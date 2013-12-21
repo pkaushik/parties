d8a63ab57fb670a7172a40d78c7548c4eb99f43d
Pallavi Anderson <pkaushik@alum.mit.edu>
2013-12-21 17:00:58 -0600

    trivial

--- server/accounts.js
+++ server/accounts.js
@@ -3,11 +3,19 @@ Accounts.loginServiceConfiguration.remove({
   service: "github"
 });
 
+// chicago-parties.meteor.com
 Accounts.loginServiceConfiguration.insert({
   service: "github",
   clientId: "5c9b7e93a5830dd3634a",
   secret: "1fd43cfe0e0311e726e239a7a1bec03f8c3c2c89"
 });
+
+// localhost:3000
+// Accounts.loginServiceConfiguration.insert({
+//   service: "github",
+//   clientId: "83f1c796a14e1f6ea61a",
+//   secret: "1c59890a41679554964713050e237ee89332731b"
+// });
 
 Accounts.onCreateUser(function(options, user){
   var accessToken = user.services.github.accessToken,
