6aa508f302bafb10a64b412cc02782fcdcb433d1
Pallavi Anderson <pkaushik@alum.mit.edu>
2013-12-21 10:13:01 -0600

    remove debug message and add correct tokens

--- server/accounts.js
+++ server/accounts.js
@@ -5,17 +5,15 @@ Accounts.loginServiceConfiguration.remove({
 
 Accounts.loginServiceConfiguration.insert({
   service: "github",
-  clientId: "83f1c796a14e1f6ea61a",
-  secret: "1c59890a41679554964713050e237ee89332731b"
+  clientId: "5c9b7e93a5830dd3634a",
+  secret: "1fd43cfe0e0311e726e239a7a1bec03f8c3c2c89"
 });
 
 Accounts.onCreateUser(function(options, user){
   var accessToken = user.services.github.accessToken,
     result,
     profile;
-  
-	console.log(accessToken);
-	
+  	
 	result = Meteor.http.get("https://api.github.com/user", {
     headers: {"User-Agent": "Meteor/1.0"},
     params: {
