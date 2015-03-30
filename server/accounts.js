185336b42c00f6282277a95471b2fe1c6bff4334
Pallavi Anderson <pkaushik@alum.mit.edu>
2015-03-29 19:41:46 -0500

    packages and accounts upgrade

--- server/accounts.js
+++ server/accounts.js
@@ -1,17 +1,17 @@
 // Accounts config
-Accounts.loginServiceConfiguration.remove({
+ServiceConfiguration.configurations.remove({
   service: "github"
 });
 
 // chicago-parties.meteor.com
-Accounts.loginServiceConfiguration.insert({
+ServiceConfiguration.configurations.insert({
   service: "github",
   clientId: "5c9b7e93a5830dd3634a",
   secret: "1fd43cfe0e0311e726e239a7a1bec03f8c3c2c89"
 });
 
-// // localhost:3000
-// Accounts.loginServiceConfiguration.insert({
+// localhost:3000
+// ServiceConfiguration.configurations.insert({
 //   service: "github",
 //   clientId: "83f1c796a14e1f6ea61a",
 //   secret: "1c59890a41679554964713050e237ee89332731b"
